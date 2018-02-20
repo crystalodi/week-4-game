48c23b60f901f771bb26b685efee222148e3b0b1
Crystal O <crystalodi@gmail.com>
2018-02-19 21:40:55 -0600

    add images for crystals. add logic to update score after user clicks on crystal. move logic to set score to tie, set random crystal values to new function startgame. add logic to update loss/win count if user ties or goes over score to win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,41 @@
 //When the page is done loading
 $(document).ready(function(){
-    //Define variable that determine whether or not the game has started.
-    var gameStarted = false;
     //Define variables that keep track of amount of wins and losses to display to user.
     var winCount = 0;
     var lossCount = 0;
     //Define variable that keeps track of score.
-    var currentScore = 0;
-    //Define a random number for each crystal, which will be the score.
-    var rubyScore = randomScore(1, 12);
-    var diamondScore = randomScore(1, 12);
-    var topazScore = randomScore(1, 12);
-    var emeraldScore = randomScore(1, 12);
-    //Define a random number which will be the total score to be below or tie with.
-    var scoreToWin = randomScore(19, 120)
-
-    console.log("Ruby Score: " + rubyScore);
-    console.log("Diamond Score: " + diamondScore);
-    console.log("Topaz Score: " + topazScore);
-    console.log("Emerald Score: " + emeraldScore);
-    console.log("Score to Win: " + scoreToWin);
-
+    var currentScore, scoreToWin;
+    startGame()
+    $(".crystal-image").on("click", function(){
+        var crystalScore = parseInt($(this).attr("value"));
+        currentScore += crystalScore;
+        $("#yourScore").html(currentScore);
+        if(currentScore === scoreToWin) {
+            //You Win
+            winCount++;
+            $("#numWins").html(winCount)
+            startGame()
+        }
+        else if(currentScore > scoreToWin) {
+            //You lose
+            lossCount++
+            $("#numLoss").html(lossCount)
+            startGame()
+        }
+        else {
+            //keep playing
+        }
+    });
+    function startGame() {
+        $("#red").attr("value", randomScore(1, 12))
+        $("#blue").attr("value", randomScore(1, 12));
+        $("#orange").attr("value", randomScore(1, 12));
+        $("#green").attr("value", randomScore(1, 12));
+        scoreToWin = randomScore(19, 120);
+        $("#scoreToMatch").html(scoreToWin);
+        currentScore = 0;
+        $("#yourScore").html(currentScore);
+    }
     function randomScore(min, max) {
         //Calculates a random number to be score for each crystal and generates random score to tie or be lower than;
         return Math.floor(Math.random() * (max - 1) + min); 
