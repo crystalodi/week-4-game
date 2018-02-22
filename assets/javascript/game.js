eb26c3c9ba17046b5b094605bba3fd9dd2ce07c0
Crystal Odi <crystal.odi@argodata.com>
2018-02-22 09:03:38 -0600

    add nested if statement in logic that determines if user has lost or won. change formula to get random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     var currentScore, scoreToWin;
     startGame()
     $(".crystal-image").on("click", function(){
+		//Each time a crystal is clicked add crystal score to user's current score and update field
         var crystalScore = parseInt($(this).attr("value"));
         currentScore += crystalScore;
         $("#yourScore").html(currentScore);
@@ -16,28 +17,30 @@ $(document).ready(function(){
             $("#numWins").html(winCount)
             startGame()
         }
-        else if(currentScore > scoreToWin) {
-            //You lose
-            lossCount++
-            $("#numLoss").html(lossCount)
-            startGame()
-        }
         else {
-            //keep playing
+			if(currentScore > scoreToWin) {
+				//You lose
+				lossCount++
+				$("#numLoss").html(lossCount)
+				startGame()
+			}
         }
     });
     function startGame() {
+		//call randomScore to get a new value for each crystal and the score player has to match in order to win.
         $("#red").attr("value", randomScore(1, 12))
         $("#blue").attr("value", randomScore(1, 12));
         $("#orange").attr("value", randomScore(1, 12));
         $("#green").attr("value", randomScore(1, 12));
         scoreToWin = randomScore(19, 120);
         $("#scoreToMatch").html(scoreToWin);
+		//Reset users current score to 0 each time the game is played.
         currentScore = 0;
         $("#yourScore").html(currentScore);
     }
     function randomScore(min, max) {
-        //Calculates a random number to be score for each crystal and generates random score to tie or be lower than;
-        return Math.floor(Math.random() * (max - 1) + min); 
+        min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 });
